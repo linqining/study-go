8813f264a2b6d47be024c1b177f728ff7f7a18f8
linqining <891680283@qq.com>
2021-10-18 02:18:43 -0700

    栈排序

--- 211018/algorithm_test.go
+++ 211018/algorithm_test.go
@@ -11,3 +11,14 @@ func TestStack_Reverse(t *testing.T) {
 	stack.Reverse()
 	t.Log(stack)
 }
+
+
+func TestStack_Sort(t *testing.T) {
+	stack := Stack{}
+	for _,item := range [5]int{59,2,43,4,5}{
+		stack.Push(item)
+	}
+	t.Log(stack)
+	stack.Sort()
+	t.Log(stack)
+}
--- 211018/algrothm.go
+++ 211018/algrothm.go
@@ -17,6 +17,11 @@ func(s *Stack) Pop()(ret int){
 	return
 }
 
+func(s *Stack) Peek()(ret int){
+	ret = s.data[s.i-1]
+	return
+}
+
 func (s *Stack) IsEmpty() bool{
 	return s.i ==0
 }
@@ -39,4 +44,31 @@ func (s *Stack) Reverse(){
 	elm := s.getAndRemoveLastElement()
 	s.Reverse()
 	s.Push(elm)
+}
+
+// 实现栈的排序
+// 允许申请一个新的栈，不允许使用其他数据结构
+func (s *Stack) Sort(){
+	nStack := Stack{}
+	elmCount :=s.i
+	for i:=elmCount;i>0;i--{
+		minCount :=0
+		minElm := s.Peek()
+		for j:=0;j<i;j++{
+			tmpElm := s.Pop()
+			if tmpElm < minElm{
+				minElm = tmpElm
+			}
+			nStack.Push(tmpElm)
+		}
+		s.Push(minElm)
+		for !nStack.IsEmpty(){
+			popElm := nStack.Pop()
+			if popElm == minElm && minCount ==0{
+				minCount ++
+				continue
+			}
+			s.Push(popElm)
+		}
+	}
 }
